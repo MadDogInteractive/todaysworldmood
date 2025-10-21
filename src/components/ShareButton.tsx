8d53b04b301b175213284db6bb1eed21f0f8531e
Samuel Pearce <pearcesamuel33@gmail.com>
2025-10-20 21:39:27 -0700

    Sanity Check

--- src/components/ShareButton.tsx
+++ src/components/ShareButton.tsx
@@ -13,8 +13,7 @@ export default function ShareButton() {
     // Web Share API (mobile & some desktop)
     if (navigator.share) {
       try {
-        await navigator.share({title,text,})
-        await navigator.share({url,})
+        await navigator.share({title,text,url})
         return
       } catch {
         /* fall through to copy */
